bedf500436f541e2556db8944a458f7764a82348
dongjin <kdj93843404@gmail.com>
2020-12-30 12:37:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <div className="App-body">
+        
+        </div>
+      </body>
+      <footer className="App-footer">
+        <img src={logo} className="App-logo" alt="logo" />
+      </footer>
     </div>
   );
 }
