b65c3ddd949d3e8fb3937c56be66dc930b64352c
dongjin <kdj93843404@gmail.com>
2020-12-30 17:23:38 +0900

    test layout

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <body>
-        <div className="App-body">
-        
+        <p> 헤더 </p>
+
+        <div className="logo-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </div>
+
+        <div className="others-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        </div>
+
+      </header>
+
+      <body className="App-body">
+          <p>본문</p>
       </body>
+
       <footer className="App-footer">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p> 푸터 </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </footer>
+
     </div>
   );
 }
