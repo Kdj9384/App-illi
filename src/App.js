43c94ae78c558ddd74bef39c39395a96e9f92d05
dongjin <kdj93843404@gmail.com>
2020-12-30 23:51:36 +0900

    nothing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <p> 헤더 </p>
+      </header>
+    )
+  }
+}
+
+class Body extends Component {
+  render() {
+    return (
+      <body className="App-body">
+          <p>본문</p>
+      </body>
+    )
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+
+      <footer className="App-footer">
+        <p> 푸터 </p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </footer>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      
-      <header className="App-header">
-        <p> 헤더 </p>
 
-        <div className="logo-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        </div>
+      <Header></Header>
 
-        <div className="others-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        </div>
+      <Body></Body>
 
-      </header>
-
-      <body className="App-body">
-          <p>본문</p>
-      </body>
-
-      <footer className="App-footer">
-        <p> 푸터 </p>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </footer>
+      <Footer></Footer>
 
     </div>
   );
