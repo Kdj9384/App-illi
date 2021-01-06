9e464d8b6cef983d16868bab35234c7fd34645b3
unknown <kdyoon08@konkuk.ac.kr>
2021-01-06 19:26:34 +0900

    font clear

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ class Header extends Component {
         <img src={illi_logo} className="illi_logo" alt="Illi Logo"/>
         <nav  className="Header-nav">
           <ul>
-            <li> Project </li>
-            <li> About </li>
-            <li> Contact </li>
+            <li className="font-Gmarket"> Project </li>
+            <li className="font-Gmarket"> About </li>
+            <li className="font-Gmarket"> Contact </li>
           </ul>
         </nav>
       </header>
@@ -28,31 +28,14 @@ class Body extends Component {
   render() {
     return (
       <div className="App-body">
-          <div className="Body-header">
+          {/* <div className="Body-header">
             <img src={back_img1} alt="background"/>
-          </div>
+          </div> */}
           <div className="Body-title">
-            일리소프트 교육
-            AR 콘텐츠를 통한 언플러그드 학습
+            <p>일리소프트 교육 </p>
+            <p>AR 콘텐츠를 통한 언플러그드 학습</p>
           </div>
           <div className="Body-content"> 
-           dksjf<br></br>kdsljfk<br></br>dslfjd<br></br>ksjalsk<br></br>
-           djfkd<br></br>lsajfkdl<br></br>sajfkdsal
-           <br></br>
-           fdksa<br></br>jfldks<br></br>jalfkd<br></br>jfklajfdkls<br></br>alfjdkslafjdkslafjdksaf
-           fdjk<br></br>ajv<br></br>fk<br></br>dslajfkds<br></br>alfjdksal<br></br>fjdkslajfkdlsajfkdlsajfkdlas
-           fj<br></br>dksalfjdksajfdlskajfkdalfjdksalfjdlksjafldkajfdklasjf
-           d<br></br><br></br><br></br><br></br><br></br>sjfkdsljfkdslfjdksjalskdjfkdlsajfkdlsajfkdsal
-           fdksajfldksjalfkdjfklajfdklsalfjdkslafjdkslafjdksaf
-           fdj<br></br><br></br>kajfkdslajfkdsalfjdksa<br></br>lfjdkslajfkdlsajfkdlsajfkdlas
-           fjdksalfjdksajfdlskajfkdalfjdks<br></br>alfjdlksjafldkajfdklasjf
-           dksjfkdsljfkdslfjdksjalskdjfkdlsajfkdlsajfkdsal
-           fdksajfldksjalfk<br></br>djfklajfdklsalfjdkslafjdkslafjdksaf
-           fdjkajfkdslajfkdsalfjdksalfjdkslajfkdlsajfkdlsajfkdlas
-           fjdksa<br></br>lfjdksajf<br></br>dlskajfkdalfjdksalf<br></br>jdlksjafldkajfdklasjf
-           <br></br><br></br><br></br><br></br><br></br><br></br>
-           <br></br><br></br><br></br><br></br><br></br>
-           <br></br><br></br><br></br><br></br>
           </div>
       </div>
     )
@@ -66,10 +49,10 @@ class Footer extends Component {
       <footer className="App-footer">
         <div className="footer-layout">
           <div className="footer-text">
-            <p> (주) 일리소프트 (734 - 81 - 01722) </p>
-            <p> 안산시 상록구 한양대학로 55(사동, 창업센터) (5공학관 지하) </p>
-            <p> 대표 안호준  HP : 010 - 4345 - 7940 </p>
-            <p> illisoftofficial@gmail.com </p>
+            <p className="font-Gmarket"> (주) 일리소프트 (734 - 81 - 01722) </p>
+            <p className="font-Gmarket"> 안산시 상록구 한양대학로 55(사동, 창업센터) (5공학관 지하) </p>
+            <p className="font-Gmarket"> 대표 안호준  HP : 010 - 4345 - 7940 </p>
+            <p className="font-Gmarket"> illisoftofficial@gmail.com </p>
           </div>
           <div className="footer-image">
             <a href=" "><img src={foo_icon1} alt="call"/></a>
